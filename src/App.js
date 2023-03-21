b7f5639e762f5314e73615186c1c724d8222db8c
AngrySusanoo <lunadev823@gmail.com>
2023-03-21 06:20:44 -0400

    presale_buysection commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    <Navbar />
   );
 }
 
